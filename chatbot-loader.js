80f050deadc90c829762ce58d63a24b1cc0ed612
mushtak <mushtakmomin200@gmail.com>
2025-12-17 13:39:30 +0530

    chatbot-loader.js added

---
+++ chatbot-loader.js
@@ -0,0 +1,13 @@
+(function () {
+  const scripts = [
+    'https://chatbot.mazeqube.com/runtime.e32b8adfe84f440f.js',
+    'https://chatbot.mazeqube.com/main.4338fb12db08e2d4.js'
+  ];
+
+  scripts.forEach(src => {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.src = src;
+    document.body.appendChild(script);
+  });
+})();
