e5d057c6bda5aede7286262d61445dce262d97b7
mushtak <mushtakmomin200@gmail.com>
2025-12-17 15:22:43 +0530

    updated code

---
+++ chatbot-element.js
@@ -0,0 +1,15 @@
+(function () {
+  if (window.__MAZEQUBE_CHAT_LOADED__) return;
+  window.__MAZEQUBE_CHAT_LOADED__ = true;
+
+  const script = document.createElement('script');
+  script.src = 'https://chatbot.mazeqube.com/chatbot-loader.js';
+  script.async = true;
+
+  script.onload = () => {
+    const el = document.createElement('mazeqube-chat');
+    document.body.appendChild(el);
+  };
+
+  document.head.appendChild(script);
+})();
